2270e3df31f9c3af574e198000f55da306e9cb57
minhajul <dev.minhajul@gmail.com>
2025-06-20 20:59:22 +0600

    [FEATURE] database with crud operation

--- backend/src/validations/userSchema.js
+++ backend/src/validations/userSchema.js
@@ -20,3 +20,14 @@ export const resetPasswordSchema = Joi.object({
   resetToken: Joi.string().required(),
   newPassword: Joi.string().min(6).required(),
 });
+
+export const userStoreSchema = Joi.object({
+  username: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+export const userUpdateSchema = Joi.object({
+  username: Joi.string().min(3).max(30),
+  email: Joi.string().email(),
+}).or("username", "email");
